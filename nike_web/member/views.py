03334e53ef9d528a0696c743417adac309e68cbd
Minsoo <zlemzlem5656@naver.com>
2020-02-18 15:14:31 +0900

    view 수정

--- nike_web/member/views.py
+++ nike_web/member/views.py
@@ -47,6 +47,11 @@ def logout(request):
     return redirect('/')
 
 def profile(request):
+    my_user_profile = Profile.objects.filter(user=request.user).first()
+    my_orders = Order.objects.filter(is_ordered=True, owner=my_user_profile)
+    context = {
+        'my_orders': my_orders
+    }
     return render(request, 'member/profile-index.html', {})
 
 def order(request):
